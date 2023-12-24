950508f9996363e627c9cad0d2a4a7565b336e7d
masukei <32535543+masukei@users.noreply.github.com>
2023-12-25 03:03:08 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 const API_KEY = 'AIzaSyCb6szaPAMuxRNP2SEeT5haDFsjHzCAakk';
-const SHEET_ID = '103hQleoRSkfpYH7_OgWZlwu-Gn1CGr7uVinylpdOCOQ';
+const SHEET_ID = '1QnSnZ4jEgMFxkmETMpAA5riCPtCkppi4RvJ9JQ5nM6g';
